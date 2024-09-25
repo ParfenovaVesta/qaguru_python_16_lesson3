1e4de42ea0222bd60cb2854ca539adbf34d94680
v.parfenova <vestenok1984@mail.ru>
2024-09-25 12:25:10 +0300

    Исправила поиск элемента в test_search_nok

--- test_search.py
+++ test_search.py
@@ -4,6 +4,6 @@ def test_search_ok(browser_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
-def test_search_ok(browser_open):
+def test_search_nok(browser_open):
     browser.element('[name="q"]').should(be.blank).type('hfga657%4hdfg463').press_enter()
-    browser.element('[id="search"]').should(have.text('По запросу hfga657%4hdfg463 ничего не найдено.'))
+    browser.element('.card-section').should(have.text('Your search - hfga657%4hdfg463 - did not match any documents.'))
